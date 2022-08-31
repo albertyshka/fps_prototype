f0d51da5459fb8ba831d520f641211e67363c3b6
Albert Dzhurakulov <adzhurakulov@2reallife.com>
2022-08-31 11:56:37 +0300

    imported rock and pistol

---
+++ Assets/Scripts/ProjectInstaller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class ProjectInstaller : MonoInstaller<ProjectInstaller>
+{
+	public override void InstallBindings()
+	{
+	}
+}
